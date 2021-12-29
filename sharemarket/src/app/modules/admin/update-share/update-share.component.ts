d5e9adfc3119cfe7b8d6c1f4269d5cdf3422faa4
thoratvaibhav265 <thoratvaibhav265@gmail.com>
2021-12-29 16:28:06 +0530

    fifth-commit

--- sharemarket/src/app/modules/admin/update-share/update-share.component.ts
+++ sharemarket/src/app/modules/admin/update-share/update-share.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Admin } from 'src/classes/admin';
+import { Share } from 'src/classes/share';
 import { ShareService } from 'src/services/share.service';
 import Swal from 'sweetalert2';
 
@@ -15,10 +16,12 @@ export class UpdateShareComponent implements OnInit {
   updateShare!: FormGroup
   adminId!:any
   shareId!:any
+  share!:Share
 
-  constructor(private shareService: ShareService,private activatedRoute:ActivatedRoute, private formBuilder: FormBuilder, private router: Router) { }
+  constructor
+  (private shareService: ShareService,private activatedRoute:ActivatedRoute, private formBuilder: FormBuilder, private router: Router) { this.initUpdateShareForm();}
 
-  initAddShareForm() {
+  initUpdateShareForm() {
     this.updateShare = this.formBuilder.group({
 
       sharename: new FormControl('', Validators.required),
@@ -30,28 +33,52 @@ export class UpdateShareComponent implements OnInit {
     })
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {  this.readShare() }
+
+  fillUpdateShareForm(share:Share)
+  {
+    this.updateShare = this.formBuilder.group({
+
+      sharename: new FormControl(share.sharename, Validators.required),
+      price: new FormControl(share.price, Validators.required),
+      quantity: new FormControl(share.quantity, Validators.required),
+      status: new FormControl('Standing', Validators.required)
+
+
+    })
+  }
 
   readShare()
   {
     this.shareId=this.activatedRoute.snapshot.paramMap.get('id');
-    this.shareService.getShareById()
-
-
-
-
-
-
-
-
-
-
+    this.shareService.getShareById(this.shareId).subscribe({
+      next: (response: any) => {
+        console.log(response);
+        if(response!=null)
+        {
+          this.share=response;
+          this.fillUpdateShareForm(this.share);
+        }
+      },
+      error: (error: any) => {
+        console.log(error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Server Offline',
+          text: 'Please start the server!',
+        });
+      },
+      complete: () => {
+        console.log('Complete');
+      },
+    })
   }
+
 
   updateShares(){
     {
-      this.adminId=Number(localStorage.getItem('adminId'));
-      this.shareService.updateShareById(this.adminId,this.updateShare.value).subscribe({
+      this.shareId=this.activatedRoute.snapshot.paramMap.get('id');
+      this.shareService.updateShareById(this.shareId,this.updateShare.value).subscribe({
         next: (response: any) => {
           console.log(response);
           if(response!=null)
